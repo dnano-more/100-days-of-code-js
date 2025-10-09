9286ac53e9356d54153c709a6707e88dca399bd6
Dnyaneshwar More <dnyaneshwarmore2500@gmail.com>
2025-10-09 19:14:24 +0530

    Day29: Explored truthy and falsy values

---
+++ Day28-29_Control_Flow/03.Truthy_Falsy.js
@@ -0,0 +1,21 @@
+const userEmail = []
+
+if (userEmail) {
+    console.log("Got user email");
+} else {
+    console.log("Dont have user email");
+}
+
+/* 
+* falsy values: false, 0, -0, BigInt, 0n, "", null, undefined, NaN 
+* 
+*  And all other are considerd as Truthy values.
+* truthy values: "0", "false", " ", [], {}, function(){}
+
+*/
+// let 
+
+// if (isArray(userEmail)) {
+//     console.log("user is Headers.!");
+    
+// }
